3e7473d95efd6287f05801142bc30f2d663b6c5d
Karlbaey <191684279+Karlbaey@users.noreply.github.com>
2025-12-07 14:45:03 +0800

    feat: add light/dark switch button
    click the button under the navbar to switch between light mode and dark mode.

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,35 @@ export const themeConfig: ThemeConfig = {
         lang: 'zh',
         toc: true,
         dateFmt: 'YYYY MMM D' // 'YYYY-MM-DD' | 'MM-DD-YYYY' | 'DD-MM-YYYY' | 'MMM D YYYY' | 'D MMM YYYY'
-    }
+    },
+    color: {
+    // default theme mode
+    mode: 'light', // light | dark | auto
+    light: {
+      // primary color
+      // used for title, hover, etc
+      // oklch color picker: https://oklch.com/
+      primary: 'oklch(25% 0.005 298)',
+      // secondary color
+      // used for post text
+      secondary: 'oklch(40% 0.005 298)',
+      // background color
+      background: 'oklch(96% 0.005 298)',
+      // highlight color
+      // used for navbar, selected text, etc
+      highlight: 'oklch(0.93 0.195089 103.2532 / 0.5)',
+    },
+    dark: {
+      // primary color
+      primary: 'oklch(92% 0.005 298)',
+      // secondary color
+      secondary: 'oklch(77% 0.005 298)',
+      // background color
+      background: 'oklch(22% 0.005 298)',
+      // highlight color
+      highlight: 'oklch(0.93 0.195089 103.2532 / 0.2)',
+    },
+  },
 }
 
 export default themeConfig
