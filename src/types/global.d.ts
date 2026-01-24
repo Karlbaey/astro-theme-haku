bd6b222bd64bc35465fb66965bf5c8e5075474cc
Karlbaey <191684279+Karlbaey@users.noreply.github.com>
2026-01-24 19:54:02 +0800

    chore: turn UnoCSS into Tailwind CSS v4 global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,22 +1,18 @@
-import type { AttributifyAttributes } from '@unocss/preset-attributify'
+/// <reference types="astro/client" />
 
 declare global {
-  namespace astroHTML.JSX {
-    interface HTMLAttributes extends AttributifyAttributes {}
-  }
-
   interface Window {
     webkitAudioContext: typeof AudioContext
   }
   interface Document {
-    startViewTransition: (updateCallback: () => void) => ViewTransition
+    startViewTransition(updateCallback: () => Promise<void> | void): ViewTransition
   }
-
   interface ViewTransition {
     finished: Promise<void>
     ready: Promise<void>
     updateCallbackDone: Promise<void>
+    skipTransition(): void
   }
 }
 
-export {}
+export { }
