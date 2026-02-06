f5eb52370b46b93f8829e91d5736e0d79361f55b
Karlbaey <191684279+Karlbaey@users.noreply.github.com>
2026-02-06 13:26:04 +0800

    style: redesign style to up-down style and delete left-right style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,16 +5,8 @@
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   ],
-  theme: {
-    colors: {
-      note: '#3b82f6',
-      tip: '#22c55e',
-      important: '#8b5cf6',
-      warning: '#f97316',
-      caution: '#ef4444',
-    },
-  },
 }
